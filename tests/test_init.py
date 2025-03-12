af65ae5555d3536fce86da7a9620e0f5dbd6bcc8
ねずみにうむ <nezumi.dev.0627@gmail.com>
2025-03-12 16:56:31 +0900

    Fix pytest

--- tests/test_init.py
+++ tests/test_init.py
@@ -10,5 +10,10 @@ from src.nezu_works_bot import hello
 @pytest.mark.parametrize("input_", [None])
 def test_hello(input_: str | None) -> None:
     """Tests that the message is as expected."""
-    expected = "Hello from nezu-works-bot! I'm a bot that can be used to automate tasks on GitHub."
-    assert hello(input_) == expected
+    expected = (
+        "Hello from nezu-works-bot! "
+        "I'm a bot that can be used to automate tasks on GitHub."
+    )
+    assert hello(input_) == expected, (  # noqa: S101
+        f"Expected {expected!r} but got {hello(input_)!r}"
+    )
