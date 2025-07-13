314eae435e2a35d65feddbcf4e848151ed28a4f2
ねずみにうむ <nezumi.dev.0627@gmail.com>
2025-07-13 16:24:10 +0900

    初期化

--- src/nezu_works_bot.py
+++
@@ -1,6 +0,0 @@
-def hello(input_: str | None) -> str:
-    """Return a hello message."""
-    return (
-        "Hello from nezu-works-bot! "
-        "I'm a bot that can be used to automate tasks on GitHub."
-    )
